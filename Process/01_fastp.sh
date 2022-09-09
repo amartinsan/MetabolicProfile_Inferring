ce889973d384da739708ed031de8c07bedb990b8
amartinsan <adrianms@ciencias.unam.mx>
2022-09-09 16:02:58 -0500

    Revert "Quality and filter script PRIMER proceso"
    This reverts commit e144c2eea1fd0c76eec279e22d15e00d1968776f.

--- Process/01_fastp.sh
+++ Process/01_fastp.sh
@@ -3,33 +3,15 @@
 
 #Quality check and filter to sample fasta
 #standarMariaSan
-#for sample in  ${sample_names[@]}
-#do
-#fastp -i "${sample_name}_1.fastq.gz" -I "${sample_name}_2.fastq.gz" -o "${sample_name}_1_Q.fastp.gz" -O  "${sample_name}_2_Q.fastp.gz" \
-#-V -w 5 -q 15 -x --poly_x_min_len=40 -g --poly_g_min_len=10 -l 40 -n 15 -P 20 -p -y -Y 30 -t 5 -T 5 --html="${sample_name}.html" &
-#done
-
-
-##### for any fasta file
-####Checar sitnaxis del Output para R1 y R2
-
-for file in *.fastq
+for sample in  ${sample_names[@]}
 do
-fastp -i $file -o out\$file
- -V \
- -w 5 \
- -q 17 \
- -g --poly_g_min_len=10 \
- -x --poly_x_min_len=20 \
- -l 50 \
- -n 15 \
- -p -P 20 \
- -y -Y 30 \
- --html=""$file"" 
+fastp -i "${sample_name}_1.fastq.gz" -I "${sample_name}_2.fastq.gz" -o "${sample_name}_1_Q.fastp.gz" -O  "${sample_name}_2_Q.fastp.gz" \
+-V -w 5 -q 15 -x --poly_x_min_len=40 -g --poly_g_min_len=10 -l 40 -n 15 -P 20 -p -y -Y 30 -t 5 -T 5 --html="${sample_name}.html" &
 done
 
 
-
-
-
-
+##### for any fasta file
+####Checar sitnaxis del Output creo que esta mal
+for file in *.fastq
+do
+fastp -q $file\1.fastq.gz* -I $file\2.fastq.gz* -o Q\$file -O Q\$file
