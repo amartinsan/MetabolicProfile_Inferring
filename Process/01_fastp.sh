b69421807360156a3b4ba26e6ff4ed19f2445767
amartinsan <adrianms@ciencias.unam.mx>
2022-09-12 16:26:54 -0500

    Qualty filter and processing script

--- Process/01_fastp.sh
+++ Process/01_fastp.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
 
-#Quality check and filter to sample fasta
-#standarMariaSan
-for sample in  ${sample_names[@]}
-do
-fastp -i "${sample_name}_1.fastq.gz" -I "${sample_name}_2.fastq.gz" -o "${sample_name}_1_Q.fastp.gz" -O  "${sample_name}_2_Q.fastp.gz" \
--V -w 5 -q 15 -x --poly_x_min_len=40 -g --poly_g_min_len=10 -l 40 -n 15 -P 20 -p -y -Y 30 -t 5 -T 5 --html="${sample_name}.html" &
-done
 
+
+
+#Quality check and filter to sample fasta
+#Process 1
 
 ##### for any fasta file
-####Checar sitnaxis del Output creo que esta mal
+####Checar sintaxis para pair-end cambia si se procesan aparte o solo es para  tener ambos pares en un archivo?####
+
+RUTA='/hd1/amartinsan/MetaINFER/MetabolicProfile_Inferring/Process'
+
+#for file in $RUTA/*.fastq
 for file in *.fastq
 do
-fastp -q $file\1.fastq.gz* -I $file\2.fastq.gz* -o Q\$file -O Q\$file
+
+	fastp -i $file -o $file\_out.fastq \
+ 		-V \
+ 		-q 17 \
+ 		-g --poly_g_min_len=10 \
+ 		-x --poly_x_min_len=20 \
+ 		-l 50 \
+ 		-n 15 \
+ 		-p -P 20 \
+ 		-y -Y 30 \
+		-w 5 \
+		--json=""$file".json" \
+ 		--html=""$file".html"
+done
+
+#Filter f duplicate sequences, PCR product or optical duplicates.
+for file in *_out.fastq
+do
+
+	cd-hit -i $file -o noDUP-$file -c 1.00 -T 5
+
+done
+
+#Final
