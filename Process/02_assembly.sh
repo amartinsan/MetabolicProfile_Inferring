13f6f3a299c987f47472af88d85a012c4a2364d0
amartinsan <adrianms@ciencias.unam.mx>
2022-09-12 21:56:52 -0500

    metaspades assembly

---
+++ Process/02_assembly.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+
+#Files
+
+
+
+for file in *.fastq
+do
+
+	echo $file
+	echo "input file  :" $file
+	NSLOTS=5
+	fastq=$file
+	#get R2 from R1 (derive)
+	fastq2="${fastq/R1/R2}"
+
+	#Assembly
+
+	spades.py --meta -1 $fastq -2 $fastq2 -o $fastq.spades  -t $NSLOTS -m 250 -k 21,33,55,77,99,111,127 --only-assembler
+
+done
