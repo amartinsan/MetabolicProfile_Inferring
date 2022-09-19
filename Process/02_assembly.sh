80d18e044955b7c5b759cbaa7a9baec879fc1e17
amartinsan <adrianms@ciencias.unam.mx>
2022-09-19 13:04:43 -0500

    Metagenome assembly and MetaQuast check

--- Process/02_assembly.sh
+++ Process/02_assembly.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-
+#assembly and qualitycheck
 
 #Files
 
@@ -16,8 +16,18 @@ do
 	#get R2 from R1 (derive)
 	fastq2="${fastq/R1/R2}"
 
-	#Assembly
-
+	#Spades_Assembly
 	spades.py --meta -1 $fastq -2 $fastq2 -o $file\_SPADES_ASSEMBLY  -t $NSLOTS -m 250 -k 21,33,55,77,99,111,127 --only-assembler
+
+
+	#MetaQuast for  quality evaluation
+	#Contigs
+	metaquast.py $file\_SPADES_ASSEMBLY/contigs.fasta -o $file\_contigsQuast -t 5
+	#Scaffolds
+	metaquast.py $file\_SPADES_ASSEMBLY/scaffolds.fasta -o $file\_scaffoldsQuast -t 5
+
+
+	#MEGAHIT ASSEMBLY make option in nextflow
+	#megahit -1 $fastq -2 $fastq2 -t 5 --k-list 21,33,55,77,99,111,127 -o $file\_MEGA_ASSEMBLY
 
 done
