a1f2d67ee4342c2d0179107b6ee5bf47778c4ff0
amartinsan <adrianms@ciencias.unam.mx>
2022-09-22 17:32:36 -0500

    Gene prediction

--- Process/04_prodigal.sh
+++ Process/04_prodigal.sh
@@ -6,8 +6,8 @@ do
 	prodigal -i $file\_SPADES_ASSEMBLY/contigsCDHIT.fasta -o $file\_SPADES_ASSEMBLY/spades.genes -a $file\_SPADES_ASSEMBLY/protein_spades.fasta -p meta
 
 	mkdir $file\_SPADES_ASSEMBLY/prodigal
-	mv $file\_SPADES_ASSEMBLY/spades.genes prodigal
-	mv $file\_SPADES_ASSEMBLY/protein_spades.fasta prodigal
+	mv $file\_SPADES_ASSEMBLY/spades.genes $file\_SPADES_ASSEMBLY/prodigal/
+	mv $file\_SPADES_ASSEMBLY/protein_spades.fasta $file\_SPADES_ASSEMBLY/prodigal/
 
 
 done
