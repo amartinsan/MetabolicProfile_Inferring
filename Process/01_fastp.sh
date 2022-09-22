5a72fc59fbbe92c93064df2256b557630e618580
amartinsan <adrianms@ciencias.unam.mx>
2022-09-22 17:16:00 -0500

    Quality filter preprocces

--- Process/01_fastp.sh
+++ Process/01_fastp.sh
@@ -15,7 +15,7 @@ do
         fastq2="${fastq/R1/R2}"
 
 
-	fastp -i $fastq -I $fastq2 -o out.$fastq -O out.$fastq2 \
+	fastp -i $fastq -I $fastq2 -o Q_$fastq -O Q_$fastq2 \
  		-V \
  		-q 17 \
  		-g --poly_g_min_len=10 \
@@ -31,18 +31,19 @@ done
 
 #Filter f duplicate sequences, PCR product or optical duplicates.
 #WE NEED cd-hit-dup for pair end data
-for file in out*
-do
+#for file in Q_*
+#do
+#
+#	cd-hit -i $file -o CD-$file -c 1.00 -T 5
 
-	cd-hit -i $file -o CD-$file -c 1.00 -T 5
-
-done
+#done
 
 mkdir quality
 mv *json quality/
 mv *html quality/
 mv *.fastq quality/
-mv quality/CD-* .
+#mv quality/CD-* .
 mv *clstr quality/
+mv quality/Q_* .
 
 #Final
