02861c59ce305f4f497133e6e9bb631272e299cf
Adrian Mtz <4dri4nms@gmail.com>
2022-10-12 15:48:36 -0500

    Update 03_bining.sh

--- Process/03_bining.sh
+++ Process/03_bining.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-
-
 #make index files
-	#Can also use bbmaper
 
-	#define prefix
-
-for file in *R1.fastq
+for file in *1.fastq.gz
 do
 
         NSLOTS=5
         fastq=$file
         #get R2 from R1 (derive)
-        fastq2="${fastq/R1/R2}"
-
+        fastq2="${fastq/_1/_2}"
 
 	cd $file\_SPADES_ASSEMBLY
-
-	bwa index contigsCDHIT.fasta -p bwa-
-	bwa mem bwa- ../$file ../$file2 > output.sam
-	samtools sort output.sam -o output.bam
-	samtools index  output.bam
-	#Make depth file
-	jgi_summarize_bam_contig_depths --outputDepth depth output.bam
+	
+	bowtie2-build scaffolds.fasta scaffolds.fasta.index 
+	bowtie2 --very-sensitive -t --sam-no-qname-trunc -x scaffolds.fasta.index -1 ../$file -2 ../$file2 -S scaffolds.sam --threads 15
+	samtools view -bS scaffolds.sam -o scaffolds.bam
+	samtools sort scaffolds.bam -o  scaffolds.sort.bam
+	samtools index scaffolds.sort.bam
+	#Get stats
+	samtools idxstats  contigs.sort.bam > contigs.sort_STATS.txt
+	#move or delete intermediate files
+	mkdir bowtieINDEX
+	mv *.bt2 bowtieINDEX/
+	jgi_summarize_bam_contig_depths --outputDepth depth contigs.sort.bam
 	metabat -i  contigs.fasta  -a depth -o binning
-	#Run metabatat with index
-	#runMetaBat.sh -m 1500 contigs.fasta output.bam -o bins
-	mkdir bwaOUT
-	mv bwa-* bwaOUT
-
+	#runMetaBat.sh -m 1500 contigs.fasta contigs.sort.bam -o bins
+	
+	#CHECKM
+	#checkm lineage_wf bins -x .fa checkm_bins
 
 done
+
+
