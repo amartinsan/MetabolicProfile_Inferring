5d6510332ec72ae2e80846b933d3998f99625944
Adrian Mtz <4dri4nms@gmail.com>
2022-11-05 22:16:32 -0600

    Update 04_prodigal.sh

--- Process/04_prodigal.sh
+++ Process/04_prodigal.sh
@@ -3,7 +3,7 @@
 
 for file in *R1.fastq
 do
-	prodigal -i $file\_SPADES_ASSEMBLY/contigsCDHIT.fasta -o $file\_SPADES_ASSEMBLY/spades.genes -a $file\_SPADES_ASSEMBLY/protein_spades.fasta -p meta
+	prodigal -i $file\_SPADES_ASSEMBLY/contigs.fasta -o $file\_SPADES_ASSEMBLY/spades.genes -a $file\_SPADES_ASSEMBLY/protein_spades.fasta -p meta
 
 	mkdir $file\_SPADES_ASSEMBLY/prodigal
 	mv $file\_SPADES_ASSEMBLY/spades.genes $file\_SPADES_ASSEMBLY/prodigal/
